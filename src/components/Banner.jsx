576cc1a2e200e9def4ba7e22af0de27efc18650f
oliflox <83953551+oliflox@users.noreply.github.com>
2024-11-21 01:44:44 +0100

    Component Banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,7 @@
+export default function Banner() {
+    return (
+        <section class="banner">
+                <h1 class="banner-title">Chez vous, partout et ailleurs</h1>
+         </section> 
+    )
+}
