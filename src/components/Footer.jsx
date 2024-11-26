b785f22999e2300a27bbf5b4222a7ae0b9c58fe9
oliflox <83953551+oliflox@users.noreply.github.com>
2024-11-26 14:24:13 +0100

    Create Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import Logo from "./Logo"
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <Logo />
+            <p>© 2020 Kasa. All rights reserved</p>
+        </footer>
+    )
+}
