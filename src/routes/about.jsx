ef12c18f8939d517e9e8842d51d8ded6e659568d
oliflox <83953551+oliflox@users.noreply.github.com>
2024-11-26 14:24:06 +0100

    Change about layout

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,9 +1,12 @@
 import Header from '../components/Header';
+import Layout from '../components/Layout';
 
 export default function About() {
     return (
       <>
-        <Header />
+        <Layout>
+          
+        </Layout>
       </>
     );
   }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,13 @@
-import Header from "../components/Header";
 import Banner from "../components/Banner";
 import Gallery from "../components/Gallery";
+import Layout from "../components/Layout";
 
 export default function Index() {
     return (
-      <>
-        <Header />
+      <Layout>
         <Banner />
         <Gallery />
-      </>
+      </Layout>
     );
   }
   
