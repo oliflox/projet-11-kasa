8fa1e168a568d11a4eb7389e064d255589dcc98b
oliflox <83953551+oliflox@users.noreply.github.com>
2024-11-27 13:45:27 +0100

    Complete 404 Error page

--- src/routes/Error404.jsx
+++ src/routes/Error404.jsx
@@ -1,7 +1,17 @@
+import Layout from "../components/Layout";
+
 export default function Error404() {
-    return (
-      <>
-      <p>Error 404</p>
-      </>
-    );
-  }
+  return (
+    <Layout>
+      <section className="E404-container">
+        <h1 className="E404-title">404</h1>
+        <p className="E404-description">
+          Oups! La page que vous demandez n'existe pas.
+        </p>
+        <a className="E404-button" href="/">
+          Retourner sur la page d’accueil
+        </a>
+      </section>
+    </Layout>
+  );
+}
