c23fcc91124a1f5d6ebdf390e66017e8fdd8db5e
oliflox <83953551+oliflox@users.noreply.github.com>
2024-11-27 13:57:28 +0100

    make the component Logo reusable

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Logo from "./Logo"
 export default function Footer() {
     return (
         <footer className="footer">
-            <Logo />
+            <Logo link="logo-blanc.png" height="2.5rem" width="7.5rem"/>
             <p>© 2020 Kasa. All rights reserved</p>
         </footer>
     )
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,11 @@
-export default function Logo() {
+export default function Logo({ link, height, width }) {
   return (
     <a href="/">
       <img
-        src={`${process.env.PUBLIC_URL}/img/logo.png`}
-        alt="logo du site kasa"
+        src={`${process.env.PUBLIC_URL}/img/${link}`}
+        alt="Logo du site Kasa"
+        style={{ width: `${width}`, height: `${height}` }}
       />
     </a>
-
-  )
+  );
 }
