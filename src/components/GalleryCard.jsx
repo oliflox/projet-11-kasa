4ee111d52ec2605e753ec7b815a8901b836f6d65
oliflox <83953551+oliflox@users.noreply.github.com>
2024-11-27 13:45:07 +0100

    Send ID in the URL

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -1,12 +1,14 @@
-export default function GalleryCard({ title, cover }) {
-
-    return (
-        <section className="gallery-card"> 
-        <img className="gallery-card__img" src={cover} alt={title}/>
-        <div className="gallery-card__overlay">
-            <h2 className="gallery-card__title">{title}</h2>
-        </div>
-            
-        </section>
-    );
+export default function GalleryCard({ title, cover, id }) {
+  const dsq = () => {
+    console.log(title);
+    window.location.href = `/logement?id=${id}`;
+  };
+  return (
+    <section className="gallery-card" onClick={dsq}>
+      <img className="gallery-card__img" src={cover} alt={title} />
+      <div className="gallery-card__overlay">
+        <h2 className="gallery-card__title">{title}</h2>
+      </div>
+    </section>
+  );
 }
