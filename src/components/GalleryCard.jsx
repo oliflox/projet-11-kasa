b1b358139de9d7ea8cc9d702e5c794ff82817a1c
oliflox <83953551+oliflox@users.noreply.github.com>
2025-05-12 18:48:16 +0200

    Refactor Stars and GalleryCard components: update styles for stars alignment and improve navigation handling

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -2,13 +2,12 @@ import { useNavigate } from "react-router-dom";
 
 export default function GalleryCard({ title, cover, id }) {
   const navigate = useNavigate();
-  const dsq = () => {
-    console.log(title);
+  const handleNavigation = () => {
     navigate(`/logement`, { state: { id } });
   };
-  
+
   return (
-    <section className="gallery-card" onClick={dsq}>
+    <section className="gallery-card" onClick={handleNavigation}>
       <img className="gallery-card__img" src={cover} alt={title} />
       <div className="gallery-card__overlay">
         <h2 className="gallery-card__title">{title}</h2>
--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -4,12 +4,13 @@ export default function Stars({ rating }) {
       {[1, 2, 3, 4, 5].map((star) => {
         return (
           <span
-            className="stars"style={{
+            key={star} 
+            className="stars"
+            style={{
               color: rating >= star ? "var(--primary)" : "var(--lightgray)",
             }}
           >
-            {" "}
-            ★{" "}
+            ★
           </span>
         );
       })}
